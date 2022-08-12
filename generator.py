b9f7a2035fa668868188004c662544bf383e0bcd
Abda Rafi <abda_rafi@yahoo.com>
2022-08-12 12:27:45 +0000

    random by os lib to support older python3

--- generator.py
+++ generator.py
@@ -1,7 +1,7 @@
-import base64, hashlib, random
+import base64, hashlib, os
 
 def code_verifier():
-    return base64.b64encode(random.randbytes(50)).decode('utf-8').replace("+", "-").replace("/", "_").replace("=", "") 
+    return base64.b64encode(os.urandom(50)).decode('utf-8').replace("+", "-").replace("/", "_").replace("=", "") 
 
 def code_challenge(code_verifier):
     return base64.b64encode(hashlib.sha256(bytes(code_verifier, encoding='utf8')).digest()).decode('utf-8').replace("+", "-").replace("/", "_").replace("=", "")
