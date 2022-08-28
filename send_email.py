4109f5d655cd099f908302ec641d174b1a1303ea
Abda Rafi <abda_rafi@yahoo.com>
2022-08-28 00:24:47 +0000

    up: convert to func

--- send_email.py
+++ send_email.py
@@ -1,26 +1,28 @@
 import smtplib, datetime
 
-smtp_user = 'example'
-smtp_password = 'examplepassword'
+def send(message: str):
+    smtp_user = 'example'
+    smtp_password = 'examplepassword'
 
-sent_from = "Example <example@example.com>"
-to = ['example_recipient@example.com']
-subject = '[UPDATES] Daily Internet Quota - {}'.format(datetime.datetime.now().date())
-body = "Body"
+    sent_from = "Example <example@example.com>"
+    to = ['example_recipient@example.com']
+    subject = '[UPDATES] Daily Internet Quota - {}'.format(datetime.datetime.now().date())
+    body = message
 
-email_text = """\
-From: %s
-To: %s
-Subject: %s
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
 
-%s
-""" % (sent_from, ", ".join(to), subject, body)
-try:
-    server = smtplib.SMTP('mail.smtp.com', 2525)
-    server.login(smtp_user, smtp_password)
-    server.sendmail(sent_from, to, email_text)
-    server.close()
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+    
+    try:
+        server = smtplib.SMTP('mail.smtp.com', 2525)
+        server.login(smtp_user, smtp_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
 
-    print('Email sent!')
-except Exception as e:
-    print('Something went wrong with sending email: {}'.format(str(e)))
+        print('Email sent!')
+    except Exception as e:
+        print('Something went wrong with sending email: {}'.format(str(e)))
